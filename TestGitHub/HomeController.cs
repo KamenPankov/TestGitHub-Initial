7b13b47859d32ef2bef44e83b213f168f5c331f3
Kamen Pankov <kamen.pankov@gmail.com>
2020-02-16 19:03:01 +0200

    Moved HomeController

---
+++ TestGitHub/HomeController.cs
@@ -0,0 +1,36 @@
+﻿namespace Andreys.App.Controllers
+{
+    using Andreys.Services.Products;
+    using Andreys.ViewModels.Products;
+    using SIS.HTTP;
+    using SIS.MvcFramework;
+
+    public class HomeController : Controller
+    {
+        private readonly IProductsService productsService;
+
+        public HomeController(IProductsService productsService)
+        {
+            this.productsService = productsService;
+        }
+
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (!this.IsUserLoggedIn())
+            {
+                return this.View();
+            }
+            else
+            {
+                ProductsListViewModel productsListViewModel = new ProductsListViewModel()
+                {
+                    Products = this.productsService.All()
+                };
+
+                return this.View(productsListViewModel, "Home");
+            }
+            
+        }
+    }
+}
